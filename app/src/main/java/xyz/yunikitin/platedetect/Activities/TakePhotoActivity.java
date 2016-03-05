e9efd1ce9271638737ca815f2e3582a3113e8df5
urboom <yurec116@gmail.com>
2016-03-05 23:09:33 +0200

    SD

--- app/src/main/java/xyz/yunikitin/platedetect/Activities/TakePhotoActivity.java
+++ app/src/main/java/xyz/yunikitin/platedetect/Activities/TakePhotoActivity.java
@@ -1,25 +1,25 @@
 package xyz.yunikitin.platedetect.Activities;
 
+
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
 import xyz.yunikitin.platedetect.R;
 
 public class TakePhotoActivity extends AppCompatActivity {
 
     static final int GALLERY_REQUEST = 1;
-
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +27,7 @@ public class TakePhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_take_photo);
 
         Button openButton = (Button)findViewById(R.id.open_button);
+        imageView = (ImageView)findViewById(R.id.open_plate_imageView);
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,23 +41,70 @@ public class TakePhotoActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == GALLERY_REQUEST) {
+            Uri dataUri = data.getData();
+            int w = imageView.getWidth();
+            int h = imageView.getHeight();
 
-        Bitmap bitmap = null;
-        ImageView imageView = (ImageView) findViewById(R.id.open_plate_imageView);
+            try {
+                Bitmap bm = loadScaledBitmap(dataUri, w, h);
+                imageView.setImageBitmap(bm);
 
-        switch(requestCode) {
-            case GALLERY_REQUEST:
-                if(resultCode == RESULT_OK){
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    imageView.setImageBitmap(bitmap);
-                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    /*
+    reference:
+    Load scaled bitmap
+    http://android-er.blogspot.com/2013/08/load-scaled-bitmap.html
+     */
+    private Bitmap loadScaledBitmap(Uri src, int req_w, int req_h) throws FileNotFoundException {
+
+        Bitmap bm = null;
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getBaseContext().getContentResolver().openInputStream(src),
+                null, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, req_w, req_h);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        bm = BitmapFactory.decodeStream(
+                getBaseContext().getContentResolver().openInputStream(src), null, options);
+
+        return bm;
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options,
+                                     int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and
+            // width
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will
+            // guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
     }
 }
